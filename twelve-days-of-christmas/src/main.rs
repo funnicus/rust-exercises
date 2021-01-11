79305f655b625bb84735cd7bbf6c59fc90065431
Juhana Kuparinen <jmkupa@utu.fi>
2021-01-11 13:45:26 +0200

    twelve days of christmas

--- twelve-days-of-christmas/src/main.rs
+++ twelve-days-of-christmas/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 1..=12 {
+        if i == 1{
+            println!("On the 1st day of Christmas");
+        }
+        else if i == 2 {
+            println!("On the 2nd day of Christmas");
+        }
+        else {
+            println!("On the {}th day of Christmas", i);
+        }
+        println!("my true love gave me");
+        println!("{} something something...", i);
+    }
 }
